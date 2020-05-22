4d491b92bb27904900030ab1c4ca58947a3ab9e7
Axel <axel.guza@etu.unilim.fr>
2020-05-22 17:16:07 +0200

    Ajout des premier scène et menu
    - Scène avec porte 
    - Menu

---
+++ Project_LNPQ/Assets/Script/chang_scene.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class chang_scene : MonoBehaviour
+{
+  
+    public void chargerScene(string MapAchanger)
+    {
+        SceneManager.LoadScene(MapAchanger);
+    }
+
+  
+
+  
+}
---
+++ Project_LNPQ/Assets/Script/quitter.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class quitter : MonoBehaviour
+{
+   public void Pourquitter()
+    {
+        Debug.Log("has left ce game");
+        Application.Quit();
+    }
+}
