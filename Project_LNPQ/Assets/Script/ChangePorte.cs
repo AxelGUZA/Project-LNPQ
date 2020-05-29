b7c8814cb3ed3d351d04871d586a052219e0da0b
Axel <axel.guza@etu.unilim.fr>
2020-05-29 15:49:37 +0200

    Ajout des couloir aléatoire et des porte
    - Portail fonctionnel
    - changement de salle
    - Porte du boss

--- Project_LNPQ/Assets/Script/ChangePorte.cs
+++ Project_LNPQ/Assets/Script/ChangePorte.cs
@@ -6,107 +6,214 @@ using UnityEngine.UI;
 public class ChangePorte : MonoBehaviour
 {
     public Sprite[] spritePorte;
-    public int textureCourante;
+    public int textureCourantePorte;
+
+    public Sprite[] spriteCouloir;
+    private string nomDePorte = null;
+
+    public GameObject gameObjectCouloir;
+
 
     public GameObject gameObjectPorte;
     public GameObject gameObjectPorte2;
     public GameObject gameObjectPorte3;
-    private string nomdeLaPorte;
+
+ 
+ 
+
+    public Text etage_text;
 
     private int ramdomNombre;
 
 
+
+    public void clickporte1()
+    {
+        this.nomDePorte = gameObjectPorte.GetComponent<Image>().sprite.ToString();
+        Debug.Log("nom de la porte cliquer 1: " + gameObjectPorte.GetComponent<Image>().sprite.ToString());
+    }
+
+
+
+
+    public void clickPorte2()
+    {
+        this.nomDePorte = gameObjectPorte2.GetComponent<Image>().sprite.ToString();
+        Debug.Log("nom de la porte cliquer 2 : " + gameObjectPorte2.GetComponent<Image>().sprite.ToString());
+    }
+
+
+
+    public void clickPorte3()
+    {
+        this.nomDePorte = gameObjectPorte3.GetComponent<Image>().sprite.ToString();
+        Debug.Log("nom de la porte cliquer 3 : " + gameObjectPorte3.GetComponent<Image>().sprite.ToString());
+    }
+
+
+
+
+    public void etageSuivant()
+    {
+
+        int etage = PlayerPrefs.GetInt("etage");
+        etage++;
+
+        if (nomDePorte == "porte_violet (UnityEngine.Sprite)")
+        {
+            gameObjectCouloir.GetComponent<Image>().sprite = spriteCouloir[3];
+            etage = Random.Range(1, 9);
+
+            PlayerPrefs.SetInt("etage", etage);
+            Debug.Log("Etage : " + etage);
+            activePortecote();
+        }
+
+        if (etage == 10)
+        {
+            nomDePorte = "porte_boss";
+            desactivePortecote();
+        }
+        else if (etage > 10)
+        {
+            etage = 1;
+            activePortecote();
+        }
+
+
+        PlayerPrefs.SetInt("etage", etage);
+        etage_text.text = etage + "";
+
+
+
+        if (nomDePorte == "porte_boss")
+        {
+            gameObjectCouloir.GetComponent<Image>().sprite = spriteCouloir[4];
+            nomDePorte = null;
+        }
+        else if (nomDePorte == "porte_rouge (UnityEngine.Sprite)")
+        {
+            gameObjectCouloir.GetComponent<Image>().sprite = spriteCouloir[0];
+            activePortecote();
+        }
+        else if (nomDePorte == "porte_blue (UnityEngine.Sprite)")
+        {
+            gameObjectCouloir.GetComponent<Image>().sprite = spriteCouloir[1];
+            activePortecote();
+
+        }
+        
+        else if (nomDePorte == "porte_jaune (UnityEngine.Sprite)")
+        {
+            gameObjectCouloir.GetComponent<Image>().sprite = spriteCouloir[2];
+            desactivePortecote();
+        }
+      
+    }
+
+
+
+
+    private void activePortecote()
+    {
+        gameObjectPorte2.SetActive(true);
+        gameObjectPorte3.SetActive(true);
+    }
+
+    private void desactivePortecote()
+    {
+        gameObjectPorte2.SetActive(false);
+        gameObjectPorte3.SetActive(false);
+    }
+
+
+
+
     void Start()
     {
-        randomPorte();
-        gameObjectPorte.GetComponent<Image>().sprite = spritePorte[this.textureCourante];
-        this.nomdeLaPorte = gameObjectPorte.GetComponent<Image>().sprite.ToString();
-        randomPorte();
-        gameObjectPorte2.GetComponent<Image>().sprite = spritePorte[this.textureCourante];
-        this.nomdeLaPorte = gameObjectPorte.GetComponent<Image>().sprite.ToString();
-        randomPorte();
-        gameObjectPorte3.GetComponent<Image>().sprite = spritePorte[this.textureCourante];
-        this.nomdeLaPorte = gameObjectPorte.GetComponent<Image>().sprite.ToString();
+
+        PlayerPrefs.SetInt("etage", 1);
+        randomiseLesPorte();
     }
+
+        
 
     public void randomiseLesPorte()
     {
 
+       
+        randomPorte();
+        gameObjectPorte.GetComponent<Image>().sprite = spritePorte[this.textureCourantePorte];
+
+
 
         randomPorte();
-        gameObjectPorte.GetComponent<Image>().sprite = spritePorte[this.textureCourante];
-        this.nomdeLaPorte = gameObjectPorte.GetComponent<Image>().sprite.ToString();
+        gameObjectPorte2.GetComponent<Image>().sprite = spritePorte[this.textureCourantePorte];
+  
+
+        
         randomPorte();
-        gameObjectPorte2.GetComponent<Image>().sprite = spritePorte[this.textureCourante];
-        this.nomdeLaPorte = gameObjectPorte.GetComponent<Image>().sprite.ToString();
-        randomPorte();
-        gameObjectPorte3.GetComponent<Image>().sprite = spritePorte[this.textureCourante];
-        this.nomdeLaPorte = gameObjectPorte.GetComponent<Image>().sprite.ToString();
+        gameObjectPorte3.GetComponent<Image>().sprite = spritePorte[this.textureCourantePorte];
+
     }
+
 
 
 
 
     public void randomPorte()
     {
-
+        int etage = PlayerPrefs.GetInt("etage");
         ramdomNombre = Random.Range(0, 100);
 
         if (ramdomNombre >= 0 && ramdomNombre <= 50)
-            this.textureCourante = 1;
-        else if (ramdomNombre > 50 && ramdomNombre <= 93)
-            this.textureCourante = 2;
-        else if (ramdomNombre > 93 && ramdomNombre <= 94)
-            this.textureCourante = 3;
+            this.textureCourantePorte = 1;
+        else if (ramdomNombre > 50 && ramdomNombre <= 90)
+            this.textureCourantePorte = 2;
+        else if (ramdomNombre > 90 && ramdomNombre <= 94)
+            this.textureCourantePorte = 3;
         else if (ramdomNombre > 94 && ramdomNombre <= 100)
-            this.textureCourante = 4;
+            this.textureCourantePorte = 4;
+
+        if (etage == 9)
+        {
+            textureCourantePorte = 5;
+        }
 
 
-        switch (textureCourante)
+            switch (textureCourantePorte)
         {
             case 1:
-                textureCourante = 1;
-                this.textureCourante %= spritePorte.Length;
+                textureCourantePorte = 1;
+                this.textureCourantePorte %= spritePorte.Length;
                 break;
             case 2:
-                textureCourante = 2;
-                this.textureCourante %= spritePorte.Length;
+                textureCourantePorte = 2;
+                this.textureCourantePorte %= spritePorte.Length;
                 break;
 
             case 3:
-                textureCourante = 3;
-                this.textureCourante %= spritePorte.Length;
+                textureCourantePorte = 3;
+                this.textureCourantePorte %= spritePorte.Length;
                 break;
             case 4:
-                textureCourante = 4;
-                this.textureCourante %= spritePorte.Length;
+                textureCourantePorte = 4;
+                this.textureCourantePorte %= spritePorte.Length;
                 break;
             case 5:
-                textureCourante = 5;
-                this.textureCourante %= spritePorte.Length;
+                textureCourantePorte = 5;
+                this.textureCourantePorte %= spritePorte.Length;
                 break;
 
             default:
-                textureCourante = 0;
-                this.textureCourante %= spritePorte.Length;
+                textureCourantePorte = 0;
+                this.textureCourantePorte %= spritePorte.Length;
                 break;
 
 
-               
-          
+ 
         }
         
-
-       
-
-        Debug.Log("Nom de la porte : " + nomdeLaPorte);
- 
-    }
-
-    public void getNomDeLaPorte(){
-
-        Debug.Log("Nom de la porte : " + nomdeLaPorte);
-      
     }
 }
 
