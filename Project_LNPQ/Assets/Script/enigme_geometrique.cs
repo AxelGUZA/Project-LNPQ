0b499acd7ac70b854f078ff017139e28f04da949
krakazell <contact.fbource@gmail.com>
2020-06-09 00:17:13 +0200

    Auto stash before merge of "master" and "origin/master"

---
+++ Project_LNPQ/Assets/Script/enigme_geometrique.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enigme_geometrique : MonoBehaviour
+{
+    public Sprite[] tableauImages ;
+    public GameObject[] boutons ;
+
+   
+
+    private Sprite[] resultTableauImages;
+    private GameObject[] resultTableauBoutons;
+
+    private int supportRandom;
+    private int[] K = new int[5];
+    
+
+    private void randomImage()
+    {
+        int i = 0;
+        int x = Random.Range(0, tableauImages.Length);
+        do
+        {
+            for(int j =0; j < K.Length; j++)
+            {
+                if (K[j] == x)
+                  x = Random.Range(0, tableauImages.Length);
+                else
+                    K[i] = x;
+            }
+            Debug.Log("les valeur de k sont :" + K[i]);
+            i++;
+            
+
+        } while (i != tableauImages.Length);
+
+
+
+       /* int i = 0;  //indice de K[]
+            int j = i - 1;  //indice permettant d'empêcher les doublons
+            int nb = Random.Range(0, tableauImages.Length);
+            K[i] = nb;
+
+            for (i=1; i<K.Length; i++) 
+            {
+                
+                nb = Random.Range(0, tableauImages.Length);
+                if(nb == K[j])
+                {
+                    i--;
+                }
+                else
+                {
+                    K[i] = nb;
+                }
+            }*/
+        
+    }
+
+    private void randomBouton()
+    {
+
+        foreach (GameObject bouton in boutons)
+        {
+            int i = Random.Range(0, boutons.Length);
+            
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Project_LNPQ/Assets/Script/enigme_intrus.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enigmes : MonoBehaviour
+{
+
+    public static string[] ListeEnigme1 = { "nain", "elfe", "orc", "alien" };
+    public static string[] ListeEnigme2 = { "épée", "hache", "balai", "lance" };
+    public static string[] ListeEnigme3 = { "casque", "plastron", "blouson", "épaulière" };
+    string[][] tableauEnigmes = { ListeEnigme1, ListeEnigme2, ListeEnigme3 };
+
+    public static string[] ListeIntrus = { "alien", "balai", "blouson" };
+
+    private string[] enigme;
+    private int randomTableauEnigme;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        randomListeIntrus();
+
+    }
+
+    void Update()
+    {
+
+    }
+
+
+    public void randomListeIntrus()
+    {        //détermine la liste de mots de l'énigme
+        int i = tableauEnigmes.Length;
+        randomTableauEnigme = Random.Range(0, i);
+        enigme = tableauEnigmes[randomTableauEnigme];
+    }
+
+}
+
+
+
+
+/*
+ dans void start faire l'affichage de l'énigme
+ dans void update faire la fonction de résolution de l'énigme
+ */
